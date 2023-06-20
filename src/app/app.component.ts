e8d64c83f0abcf36a852e29420309ede4dffaa33
Gaming 15 <Gaming 15@PAVILION>
2023-06-20 22:42:51 +0330

    14020329

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'DatingApp';
+export class AppComponent implements OnInit {
+  users: any;
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.httpClient.get('https://localhost:7089/api/user').subscribe(
+      (Response) => {
+        this.users = Response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
